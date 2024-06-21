4d90b84b551b047c88e21fa624fd3a9a78ca4775
BBNSS <74817462+bbnss@users.noreply.github.com>
2024-06-21 21:03:21 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py
+
+# Configurazione OpenAI
+OPENAI_API_KEY = 'your_openai_API_key'
+OPENAI_MODEL = "gpt-3.5-turbo"  # o "gpt-4" se preferisci
+
+# Configurazione del prompt
+SYSTEM_PROMPT = "Setup_here_your_system_prompt"
+USER_PROMPT = "Setup_here_your_user_prompt"
+
+# Configurazione OpenAI
+MAX_TOKENS = 4096
+TEMPERATURE = 0.7
+
+# Configurazione email
+EMAIL_SENDER = "your_email@example.com"
+EMAIL_PASSWORD = "your_email_password"
+EMAIL_RECIPIENT = "recipient@example.com"
+SMTP_SERVER = "smtp.example.com"
+SMTP_PORT = 587
+
+# Lista dei canali YouTube
+YOUTUBE_CHANNELS = [
+    "https://www.youtube.com/@lexfridman/videos",
+    # Aggiungi altri canali qui
+]
